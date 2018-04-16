c09e122af431fb9357bb1dc2508f0cec36c2f95d
betty_c <betty.china00@gmail.com>
2018-04-16 14:16:08 -0700

    first commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+
+  def create
+  	user = User.authenticate(params[:session][:email], params[:session][:password])
+  	if user.nil?
+  		flash[:error] = "couldn't find a user with those credentials"
+      redirect_to new_session_path
+  	else
+  		sign_in user
+  		redirect_to user
+  	end
+  end
+
+  def destroy
+  	sign_out 
+  	redirect_to signin_path
+  end
+
+end
